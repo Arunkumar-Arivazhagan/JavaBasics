2fefd714e4c78f8394edb92000f4370e33ee4112
arun04tt <arun@turftown.in>
2022-12-01 10:11:04 +0530

    Relational Operator

---
+++ src/SecondClass/RelationalOperator.java
@@ -0,0 +1,60 @@
+package SecondClass;
+
+import java.util.Scanner;
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+		
+//		Scanner scan = new Scanner(System.in);
+//		
+//		System.out.println("Enter a value : ");
+//		
+//		int a = scan.nextInt();
+//		
+//		System.out.println("Enter b value : ");
+//		
+//		int b = scan.nextInt();
+//		
+//		System.out.println("Enter c value : ");
+//		
+//		int c = scan.nextInt();
+//		
+//		
+//		if(a==b && a==c && b==c)
+//		{
+//			System.out.println("A, B and C values are equal");
+//		}
+//		else if(a>b && a>c)
+//		{
+//			System.out.println("A is greater");
+//		}
+//		else if(b>a & b>c)
+//		{
+//			System.out.println("B is greater");
+//		} else
+//		{
+//			System.out.println("C is greater");
+//		}
+		
+		orFunction();
+	}
+	
+	static void orFunction() {
+		boolean passport = false;
+		boolean driverLic = false;
+		boolean criminal = true;
+//		if(passport || driverLic)
+//		{
+//			System.out.println("You are allowed to participate");
+//		}
+//		else 
+		if(!criminal)
+		{
+			System.out.println("You have clear records");
+		} else
+		{
+			System.out.println("You are not allowed");
+		}
+	}
+}
